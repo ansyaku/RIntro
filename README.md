b61cc9b36bb67c3ec7ca2190bf2fc185f5b19457
Andreas S K <andreas.syaloom.k@gmail.com>
2021-06-24 16:19:16 +0700

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# R Wiki
+=========================================================================================
+
+This repository is used for saving all my notes on learning R.
---
+++ contoh_file_dump.R
@@ -0,0 +1,7 @@
+x <-
+c("Kabanjahe", "Medan", "Siantar", "Samosir", "Balige", "Labuhan Batu", 
+"Padang Sidempuan", "Rantau Prapat")
+y <-
+list(TRUE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, FALSE)
+z <-
+c(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
